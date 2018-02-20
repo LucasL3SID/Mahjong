58d12c3d893a9b732a78eb0080a2a24a65d97798
LucasL3SID <34313645+LucasL3SID@users.noreply.github.com>
2018-02-20 16:31:29 +0100

    nettoyage

---
+++ Code_Site/php/Jouer.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="fr" >
+
+<head>
+<title id="index">Mahjong</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="D:\CYRIL\Données\L3_SID\projet_github\Page_Accueil\Page_Accueil.css" type="text/css" media="screen">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<body>
+<a class="bouton" href="http://localhost/l3sid/Mahjong/Page_Accueil"> Retour à la page d'accueil</a>
+
+<a class="bouton" href="http://localhost/l3sid/Mahjong/Historique.php"> Historique</a>
+
+<a class="bouton" href="http://localhost/l3sid/Mahjong/Record.php"> Record</a>
+
+<table class="tbl_form' width="500">
+<tr><td><input type="Button" value="1" class="btn_valid" /></td>
+<td><input type="Button" value="2" class="btn_valid" /></td>
+<td><input type="Button" value="3" class="btn_valid" /></td>
+<td><input type="Button" value="4" class="btn_valid" /></td>
+<td><input type="Button" value="5" class="btn_valid" /></td>
+<td><input type="Button" value="6" class="btn_valid" /></td>
+<td><input type="Button" value="7" class="btn_valid" /></td>
+<td><input type="Button" value="8" class="btn_valid" /></td>
+<td><input type="Button" value="9" class="btn_valid" /></td>
+<td><input type="Button" value="10" class="btn_valid" /></td>
+</tr>
+</table>
+</body>
+</html>
---
+++ Code_Site/php/Record.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="fr" >
+
+  <head>
+    <title id="index">Mahjong</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/var/www/l3sid/Mahjong/Page_Accueil.css" type="text/css" media="screen">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    
+  </head>
+  <body>
+
+<a class="bouton" href="http://localhost/l3sid/Mahjong/Page_Accueil"> Retour</a>
+<a class="bouton" href="http://localhost/l3sid/Mahjong/Jouer.php"> Jouer</a>
+
+
+
+</body>
+</html>

4a5aa7855e4367b722b1b8e62fbaa2d9d52328e6
CYRILGD <cyrilgd.games@gmail.com>
2018-02-22 18:58:12 +0100

    no message

--- Code_Site/php/Historique.php
+++ Code_Site/php/Historique.php
@@ -7,14 +7,14 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/var/www/l3sid/Mahjong/Page_Accueil.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="http://localhost/l3sid/projet_githubs/Code_Site/Css/Page_Accueil.css" type="text/css" media="screen">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     
   </head>
   <body>
 
-<a class="bouton" href="http://localhost/l3sid/projet_github/Code_Site/html/Page_Accueil"> Retour</a>
-<a class="bouton" href="http://localhost/l3sid/projet_github/Code_Site/php/Jouer.php"> Jouer</a>
+<a class="bouton" href="http://localhost/l3sid/projet_githubs/Code_Site/html/Page_Accueil"> Retour</a>
+<a class="bouton" href="http://localhost/l3sid/projet_githubs/Code_Site/php/Jouer.php"> Jouer</a>
 
 
 
--- Code_Site/php/Jouer.php
+++ Code_Site/php/Jouer.php
@@ -7,15 +7,15 @@
 <meta charset="utf-8">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="D:\CYRIL\Données\L3_SID\projet_github\Page_Accueil\Page_Accueil.css" type="text/css" media="screen">
+<link rel="stylesheet" href="http://localhost/l3sid/projet_githubs/Code_Site/Css/Page_Accueil.css" type="text/css" media="screen">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <body>
-<a class="bouton" href="http://localhost/l3sid/projet_github/Code_Site/html/Page_Accueil"> Retour à la page d'accueil</a>
+<a class="bouton" href="http://localhost/l3sid/projet_githubs/Code_Site/html/Page_Accueil"> Retour à la page d'accueil</a>
 
-<a class="bouton" href="http://localhost/l3sid/projet_github/Code_Site/php/Historique.php"> Historique</a>
+<a class="bouton" href="http://localhost/l3sid/projet_githubs/Code_Site/php/Historique.php"> Historique</a>
 
-<a class="bouton" href="http://localhost/l3sid/projet_github/Code_Site/php/Record.php"> Record</a>
+<a class="bouton" href="http://localhost/l3sid/projet_githubs/Code_Site/php/Record.php"> Record</a>
 
 <table class="tbl_form' width="500">
 <tr><td><input type="Button" value="1" class="btn_valid" /></td>
